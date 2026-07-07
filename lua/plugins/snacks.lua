559b5a93c6f8e97737594b6b329a3b71d2a35a5f
Masha Klimkin <m.klimkin@rasa.com>
2026-07-07 14:48:34 +0200

    update bufferline

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,6 +5,17 @@ return {
       picker = {
         hidden = false,
         ignored = true,
+        exclude = {
+          "node_modules",
+          ".git",
+          "dist",
+          "build",
+          ".yarn",
+          "*.min.js",
+          "*-lock.json",
+          "yarn.lock",
+          "*coverage*",
+        },
         sources = {
           files = {
             hidden = false,
